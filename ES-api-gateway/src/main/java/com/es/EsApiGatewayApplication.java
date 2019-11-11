ae54a080e1329a700c035a1fbf4a7187685cdf6b
Peresh babu Kavuri <pereshbabu.k@gmail.com>
2019-11-11 14:48:22 +0530

    ES API Gateway

---
+++ ES-api-gateway/src/main/java/com/es/EsApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsApiGatewayApplication.class, args);
+	}
+
+}
